a16dd76a6fa91a5409431cb1ddaa0eff6ea65f34
YanG-6668 <goch.yannn@gmail.com>
2020-09-02 21:39:52 +0300

    added page

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+// var gulp = require("gulp");
+// var sass = require("gulp-sass");
+// gulp.task("sass", function() {
+//   gulp
+//     .src("path/to/app.scss")
+//     .pipe(
+//       sass({
+//         includePaths: require("node-normalize-scss").includePaths
+//       })
+//     )
+//     .pipe(gulp.dest("path/to/output.css"));
+// });
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,58 @@
+$(document).ready(function() {
+  var link = $('.menu__link');
+  var linkActive = $('.menu__link-active');
+  var menu = $('.mobile');
+  var mobileLink = $('.mobile__link')
+
+  link.click(function() {
+    link.toggleClass('menu__link-active');
+    menu.toggleClass('mobile__active');
+  });
+
+  linkActive.click(function() {
+    link.removeClass('menu__link-active');
+  });
+
+  mobileLink.click(function() {
+    menu.toggleClass('mobile__active');
+    link.toggleClass('menu__link-active');
+  });
+
+  $('a[href^="#"]').click(function() {
+    var target = $(this).attr('href');
+    $('html, body').animate({
+      scrollTop: $(target).offset().top
+    }, 1000);
+  });
+});
+
+$(document).scroll(function() {
+  if ($(document).scrollTop() >= 100) {
+    $('.anchor').fadeIn();
+  } else {
+    $('.anchor').fadeOut();
+  }
+});
+
 'use strict';
+
+document.body.onload = function() {
+  setTimeout(function() {
+    var preloader = document.getElementById('page-preloader');
+    if (!preloader.classList.contains('done')) {
+      preloader.classList.add('done');
+    }
+  }, 1000);
+}
+
+var loader = function(event) {
+  var show = "<span> Файл готовий до відправлення!</span>";
+  var output = document.querySelector(".footer__form-file");
+
+  output.innerHTML = show;
+  output.classList.add("active");
+
+};
+
+var fileInput = document.getElementById("upload");
+fileInput.addEventListener('change', loader);
